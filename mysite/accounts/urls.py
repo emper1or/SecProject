f0bc1af631fca81264552ab4931d6870728e85e9
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-03-22 09:31:14 +0700

    add profile urls

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('verify/', views.verify, name='verify'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
     path('reset-password/', views.reset_password, name='reset_password'),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
