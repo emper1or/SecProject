3f0aade1995004b1e95fdaea22600cbb909a7d88
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-03-22 09:30:07 +0700

    edit dashboard, add profile, edit profile, remove password login user

--- mysite/accounts/static/js/script.js
+++ mysite/accounts/static/js/script.js
@@ -1,6 +1,3 @@
-
-
-// Ждем, пока весь DOM загрузится
 document.addEventListener('DOMContentLoaded', function () {
     // Находим элемент "глазика"
     const togglePassword = document.getElementById('togglePassword');
@@ -11,16 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
             const passwordInput = document.getElementById('password');
             const icon = this.querySelector('i');
 
-            // Переключаем тип поля ввода
-            if (passwordInput.type === 'password') {
-                passwordInput.type = 'text'; // Показываем пароль
-                icon.classList.remove('fa-eye');
-                icon.classList.add('fa-eye-slash'); // Меняем иконку на "глаз закрыт" (перечёркнутый)
-            } else {
-                passwordInput.type = 'password'; // Скрываем пароль
-                icon.classList.remove('fa-eye-slash');
-                icon.classList.add('fa-eye'); // Меняем иконку на "глаз открыт"
-            }
+            this.classList.toggle('fa-eye');
+            this.classList.toggle('fa-eye-slash');
         });
     }
 });
