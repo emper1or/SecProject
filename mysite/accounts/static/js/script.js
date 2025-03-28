e60f03801f673cea57e789ead65847c14435cbee
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-03-28 23:06:13 +0700

    add login eye

--- mysite/accounts/static/js/script.js
+++ mysite/accounts/static/js/script.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Находим элемент "глазика"
     const togglePassword = document.getElementById('togglePassword');
 
-    // Добавляем обработчик события на клик
     if (togglePassword) {
         togglePassword.addEventListener('click', function () {
             const passwordInput = document.getElementById('password');
             const icon = this.querySelector('i');
 
-            this.classList.toggle('fa-eye');
-            this.classList.toggle('fa-eye-slash');
+            // Переключаем тип поля ввода
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+
+            // Переключаем иконку
+            icon.classList.toggle('fa-eye');
+            icon.classList.toggle('fa-eye-slash');
+
+            // Добавляем/убираем зачеркивание
+            icon.style.textDecoration = icon.classList.contains('fa-eye-slash') ? 'line-through' : 'none';
         });
     }
 });
