301a39e0859b3d8eba757e9bc1b7fdc09d5b9613
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-04-03 13:16:56 +0700

    fix boot_details/book_detail

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from accounts.models import CustomUser
 
+
 class Author(models.Model):
+    objects = None
     name = models.CharField(max_length=100, verbose_name="Имя автора")
     bio = models.TextField(verbose_name="Биография автора", blank=True, null=True)
 
@@ -23,19 +25,6 @@ class Book(models.Model):
 class BookCover(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name="Книга")
     cover = models.ImageField(upload_to='covers/', blank=True, null=True, verbose_name="Обложка книги")
+
     def __str__(self):
         return f"Обложка для {self.book.title}"
-
-
-class Author_Test(models.Model):
-    name = models.CharField("Имя автора", max_length=200, unique=True)
-    bio = models.TextField("Биография", blank=True)
-    birth_date = models.DateField("Дата рождения", null=True, blank=True)
-    death_date = models.DateField("Дата смерти", null=True, blank=True)
-    photo = models.URLField("Фото", blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-
